340b58736eb0c14955ecf99f795dcb3d3b3d0d8f
KatrineSh <70848128+KatrineSh@users.noreply.github.com>
2020-10-01 14:35:15 +0300

    fix: add test

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,14 +4,11 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-      let t = 0;
-      let k = 0.693 / HALF_LIFE_PERIOD;
-      let valid;
-      if (typeof (sampleActivity) === 'number') {
-          t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
-          if (t > 0) { return Math.ceil(t); }
-          else { return false };
-      } else {
-          return false;
-      };
-  };
+    let t = 0;
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    if ((typeof(sampleActivity) !== 'string') || (sampleActivity === undefined) || (sampleActivity === null) || (sampleActivity === ''))
+    { return false } else {
+        t = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / k;
+        if (t > 0 && typeof(t) === 'number' && t !== NaN && t!== Infinity) { return Math.ceil(t);} else {return false};
+    };
+};
