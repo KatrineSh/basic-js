4fa4459baa7ba9dfba769852a701f3f716040194
KatrineSh <70848128+KatrineSh@users.noreply.github.com>
2020-10-04 23:33:39 +0300

    fix: add code;

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  let result = [];
+  if (Array.isArray(members)) {    
+      for (let item of members) {
+      if (typeof(item) === 'string' && item !== '' && item !== null) {
+      const arrItem = item.trim().toUpperCase();
+      result.push(arrItem[0]);
+  };
+      };} else { return false; }
+      return result.sort().join('');
+      };
