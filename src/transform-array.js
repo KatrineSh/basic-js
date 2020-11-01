676610e65591bae52c7815bc0864a891e4da4f1e
KatrineSh <70848128+KatrineSh@users.noreply.github.com>
2020-11-01 13:58:31 +0300

    fix: code

--- src/transform-array.js
+++ src/transform-array.js
@@ -5,16 +5,35 @@ module.exports = function transform(arr) {
     let newArr = arr.slice();
 
     for (let i=0; i<newArr.length; i++) {
-        if ('--discard-prev' === newArr[i]) {
-           newArr.splice( i - 1 , 2);}
-           else if  ('--discard-next' === newArr[i]) {
-               newArr.splice(i, 2); }
-           else if ('--double-next' === newArr[i]) {
-               newArr.splice(i , 1, i + 1);
+        if ('--discard-prev' === newArr[i]) 
+        {
+            if  (newArr[i - 1] !== undefined) {
+                let myVar = newArr[i - 1];
+                newArr.splice(i , 2);
+            } else { newArr.splice(i , 1);}}
+
+           else if  ('--discard-next' === newArr[i]) 
+           {
+            if  (newArr[i + 1] !== undefined) {
+                let myVar = newArr[i + 1];
+                newArr.splice(i , 2);
+            } else { newArr.splice(i , 1);}
+            }
+           else if ('--double-next' === newArr[i] )
+            {
+                if  (newArr[i + 1] !== undefined) {
+                    let myVar = newArr[i + 1];
+                    newArr.splice(i , 1, myVar);
+                } else { newArr.splice(i , 1);}
+            }
+           else if ('--double-prev' === newArr[i]) 
+           {
+               if (newArr[i - 1] !== undefined) {
+                let myVar1 = newArr[i - 1];
+                newArr.splice(i , 1, myVar1);}
+               else { newArr.splice(i , 1);}
+               
            }
-           else if ('--double-prev' === newArr[i]) {
-               newArr.splice(i , 1, i - 1 );
-           }
-    } 
+        }
     return newArr;
 }
